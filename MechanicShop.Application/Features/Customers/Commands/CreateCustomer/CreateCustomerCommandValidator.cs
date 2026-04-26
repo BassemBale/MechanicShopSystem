85c089cacae8063bd0d513faff126df4078b81fa
BassemBale <bassem.bale2@gmail.com>
2026-04-26 17:58:06 +0300

    adding get customer query

---
+++ MechanicShop.Application/Features/Customers/Commands/CreateCustomer/CreateCustomerCommandValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+
+namespace MechanicShop.Application.Features.Customers.Commands.CreateCustomer
+{
+    public class CreateCustomerCommandValidator : AbstractValidator<CreateCustomerCommand>
+    {
+        public CreateCustomerCommandValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("name is required .")
+                .MaximumLength(100);
+
+            RuleFor(x => x.Email)
+                .EmailAddress().WithMessage("Invalid Email .")
+                .MaximumLength(100);
+
+            RuleFor(x => x.PhoneNumber)
+                .NotEmpty().WithMessage("Phone Number is required .")
+                .Matches(@"^\+?\d{7,15}$").WithMessage("Phone number must be 7 - 15 digits and may start with '+' .");
+
+            RuleFor(x => x.Vehicles)
+                .NotNull().WithMessage("Vehicle list cannot be null .")
+                .Must(p => p.Count > 0).WithMessage("At least one vehicle is required");
+
+            RuleForEach(x => x.Vehicles).SetValidator(new CreateVehicleCommandValidator());
+
+        }
+    }
+}
