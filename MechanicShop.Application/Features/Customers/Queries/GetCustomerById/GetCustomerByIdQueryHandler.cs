296a51e793787e8b293b5bd90a9624fb5d7ac174
BassemBale <bassem.bale2@gmail.com>
2026-04-26 18:23:29 +0300

    Adding Get Customer By Id query

---
+++ MechanicShop.Application/Features/Customers/Queries/GetCustomerById/GetCustomerByIdQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using MechanicShop.Application.Common.Interfaces;
+using MechanicShop.Application.Features.Customers.Dtos;
+using MechanicShop.Application.Features.Customers.Mappers;
+using MechanicShop.Domain.Common.Results;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace MechanicShop.Application.Features.Customers.Queries.GetCustomerById;
+
+public class GetCustomerByIdQueryHandler(IAppDbContext context,
+                                         ILogger<GetCustomerByIdQueryHandler> logger) 
+    :IRequestHandler<GetCustomerByIdQuery, Result<CustomerDto>>
+{
+    private readonly IAppDbContext _context = context;
+    private readonly ILogger<GetCustomerByIdQueryHandler> _logger = logger;
+
+    public async Task<Result<CustomerDto>> Handle(GetCustomerByIdQuery request, CancellationToken ct)
+    {
+        var customer = await _context.Customers.AsNoTracking().Include(x => x.Vehicles)
+            .FirstOrDefaultAsync(x => x.Id == request.CustomerId, ct);
+
+        if(customer is null)
+        {
+            _logger.LogWarning("Customer with id {customerId} not found.", request.CustomerId);
+            return Error.NotFound(
+                code: "Customer_NotFound",
+                description: "$\"Customer with id '{query.CustomerId}' was not found\")"
+                );
+        }
+
+        return customer.ToDto();
+    }
+}
---
+++ MechanicShop.Application/Features/Customers/Queries/GetCustomerById/GetCustomerByIdQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace MechanicShop.Application.Features.Customers.Queries.GetCustomerById;
+
+public sealed class GetCustomerByIdQueryValidator : AbstractValidator<GetCustomerByIdQuery>
+{
+    public GetCustomerByIdQueryValidator()
+    {
+        RuleFor(x => x.CustomerId)
+            .NotEmpty()
+            .WithMessage("CustomerId is Required.")
+            .WithErrorCode("CustomerId_Is_Required");
+    }
+}
