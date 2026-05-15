3f81c1c5c3135c7d6c395ffc07192c6d040330ed
BassemBale <bassem.bale2@gmail.com>
2026-05-15 16:20:38 +0300

    Add GetLaborsQuery

---
+++ MechanicShop.Application/Features/Labors/Queries/GetLabors/GetLaborsQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using MechanicShop.Application.Common.Interfaces;
+using MechanicShop.Application.Features.Labors.Dtos;
+using MechanicShop.Application.Features.Labors.Mappers;
+using MechanicShop.Domain.Common.Results;
+using MechanicShop.Domain.Identity;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace MechanicShop.Application.Features.Labors.Queries.GetLabors;
+
+public class GetLaborsQueryHandler(IAppDbContext context) : IRequestHandler<GetLaborsQuery, Result<List<LaborDto>>>
+{
+    private readonly IAppDbContext _context = context;
+
+    public async Task<Result<List<LaborDto>>> Handle(GetLaborsQuery request, CancellationToken ct)
+    {
+        var labors = await _context.Employees.AsNoTracking().Where(x => x.Role == Role.Labor).ToListAsync(ct);
+
+        return labors.ToDtos();
+    }
+}
